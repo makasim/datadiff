2318c89c1e41459817e8d1e2996e957262c91cbf
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2015-07-24 22:26:01 +0300

    correcrly count removed\added lines.

--- frontend/client/app/main/main.controller.js
+++ frontend/client/app/main/main.controller.js
@@ -29,12 +29,16 @@ angular.module('datadiffApp')
 
 
     for (var i in commits) {
+      commits[i].removedCount = 0;
+      commits[i].addedCount = 0;
+
       if (i == 0) {
         var delta = jsondiffpatch.diff({}, commits[i].data);
-        commits[i].dataScore = getStringsCount(commits[i].data);
+        commits[i].addedCount = getStringsCount(commits[i].data);
+
       } else {
         var delta = jsondiffpatch.diff(commits[i - 1].data, commits[i].data);
-        commits[i].dataScore = getStringsCount(commits[i].data) - getStringsCount(commits[i - 1].data);
+        countChanges(commits[i - 1], commits[i]);
       }
       commits[i].htmlDiff = jsondiffpatch.formatters.html.format(delta, commits[i].data);
     }
@@ -64,4 +68,29 @@ angular.module('datadiffApp')
       return res.length;
     }
 
+    function countChanges(previousCommit, commit) {
+      for (var i in commit.diff) {
+        if (commit.diff[i].op == 'add') {
+          commit.addedCount += getStringsCount(commit.diff[i].value);
+        } else if (commit.diff[i].op == 'copy') {
+          commit.addedCount += getStringsCount(commit.data, readPath(commit.diff[i].from));
+        } else if (commit.diff[i].op == 'move') {
+          commit.addedCount += getStringsCount(commit.diff[i].value);
+          commit.removedCount += getStringsCount(commit.diff[i].value);
+        } else if (commit.diff[i].op == 'replace') {
+          commit.addedCount += getStringsCount(commit.diff[i].value);
+          commit.removedCount += getStringsCount(readPath(previousCommit.data, commit.diff[i].path));
+        } else if (commit.diff[i].op == 'remove') {
+          commit.removedCount += getStringsCount(readPath(previousCommit.data, commit.diff[i].path));
+        }
+      }
+    }
+
+    function readPath(obj, path) {
+      if (path[0] == '/') {
+        path = path.substring(1);
+      }
+
+      return path.split('/').reduce(function index(obj,i) {return obj[i]}, obj);
+    }
   });
